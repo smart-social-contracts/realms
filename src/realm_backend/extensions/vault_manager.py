0f86a4413052423a7cccdad64e5e4fdc9944b223
Jose Perez <jmpmonteagudo@gmail.com>
2025-05-21 22:59:01 +0200

    chore: add flake8 config and clean up type hints and imports across codebase

--- src/realm_backend/extensions/vault_manager.py
+++ src/realm_backend/extensions/vault_manager.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, Optional
 
 import core.candid_types_vault as vault_candids
 from core.extensions import Extension, ExtensionPermission, extension_registry
