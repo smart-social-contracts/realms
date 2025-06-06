e08f9bddd42571f920db326a2ab9fa64452ebc2e
Jose Perez <docujovilujo@gmail.com>
2025-06-06 13:41:58 +0200

    feat: add devcontainer config and remove unused extension modules

--- src/realm_backend/extensions/extension_imports.py
+++
@@ -1,5 +0,0 @@
-import extensions.citizen_dashboard.entry
-import extensions.demo_loader.entry
-import extensions.llm_chat.entry
-import extensions.test_bench.entry
-import extensions.vault_manager.entry
