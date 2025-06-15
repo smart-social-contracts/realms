1b08d11391238955ac231d7a9f9c930365c087d3
Jose Perez <jmpmonteagudo@gmail.com>
2025-06-15 21:18:43 +0200

    fix: rename UserRole to UserProfile for consistency and clarity in user role management

--- src/realm_backend/ggg/__init__.py
+++ src/realm_backend/ggg/__init__.py
@@ -24,7 +24,7 @@ from .trade import Trade
 from .transfer import Transfer
 from .treasury import Treasury
 from .user import User
-from .user_role import UserRole
+from .user_role import UserProfile
 from .vote import Vote
 
 __all__ = [
@@ -42,7 +42,7 @@ __all__ = [
     "TaskSchedule",
     "Trade",
     "User",
-    "UserRole",
+    "UserProfile",
     "Vote",
     "Transfer",
     "Human",
--- src/realm_backend/ggg/user_role.py
+++ src/realm_backend/ggg/user_role.py
@@ -4,7 +4,30 @@ from kybra_simple_logging import get_logger
 logger = get_logger("entity.user_role")
 
 
-class UserRole(Entity, TimestampedMixin):
+class Operations:
+    ALL = 'all'
+
+    USER_ADD = 'user.add'
+    USER_EDIT = 'user.edit'
+    USER_DELETE = 'user.delete'
+
+    ORGANIZATION_ADD = 'organization.add'
+    ORGANIZATION_EDIT = 'organization.edit'
+    ORGANIZATION_DELETE = 'organization.delete'
+
+    TRANSFER_CREATE = 'transfer.create'
+    TRANSFER_REVERT = 'transfer.delete'
+
+    TASK_CREATE = 'task.create'
+    TASK_EDIT = 'task.edit'
+    TASK_DELETE = 'task.delete'
+    TASK_RUN = 'task.run'
+    TASK_SCHEDULE = 'task.schedule'
+    TASK_CANCEL = 'task.cancel'
+
+
+class UserProfile(Entity, TimestampedMixin):
     __alias__ = "name"
     name = String(max_length=256)
     description = String(max_length=256)
+    allowed_to = String()
