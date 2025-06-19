c8e9c2fc540c4e9da13f0e97205314e623064bad
Jose Perez <jmpmonteagudo@gmail.com>
2025-06-19 18:38:33 +0200

    Stop tracking config.js, now generated during build

--- src/realm_frontend/src/lib/config.js
+++
@@ -1,7 +0,0 @@
-// This file is generated during the build process - do not edit manually
-// It contains canister IDs and other configuration for the application
-
-export const CANISTER_IDS = { internet_identity: '' };
-
-export const DEV_PORT = 8000;
-
