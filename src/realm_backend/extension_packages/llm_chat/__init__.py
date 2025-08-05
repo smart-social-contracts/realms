701d4c6085f4e004e40ef4d2d347ca5bad36d58a
jmpmonteagudo <jmpmonteagudo@gmail.com>
2025-08-05 19:06:00 +0200

    chore: remove unused extensions and localization files

--- src/realm_backend/extension_packages/llm_chat/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-AI assistant extension for Realm backend
-"""
--- src/realm_frontend/src/routes/(sidebar)/extensions/land_registry/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import LandRegistry from '$lib/extensions/land_registry/LandRegistry.svelte';
-</script>
-
-<svelte:head>
-  <title>Land Registry - Realm</title>
-</svelte:head>
-
-<LandRegistry />
