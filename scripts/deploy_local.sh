ffc1a45785be88499151b5b1c9785575530dd03a
jmpmonteagudo <jmpmonteagudo@gmail.com>
2025-10-09 07:53:29 +0200

    fix deployment script hanging

--- scripts/deploy_local.sh
+++ scripts/deploy_local.sh
@@ -30,7 +30,7 @@ scripts/download_wasms.sh
 
 lsof -ti:$PORT | xargs kill -9 2>/dev/null || true
 dfx stop 2>/dev/null || true
-dfx start --clean --background --host 127.0.0.1:$PORT --logfile dfx.log
+dfx start --clean --background --host 127.0.0.1:$PORT --logfile dfx.log >/dev/null 2>&1
 dfx deploy internet_identity
 dfx deploy vault
 dfx deploy realm_registry_backend --yes
