a60e98f136d111b1359ed29e1f47d5e7ff060795
deucalioncodes <jmpmonteagudo@gmail.com>
2025-11-05 18:51:19 +0100

    clean: remove no longer used vault wasm from download script

--- scripts/download_wasms.sh
+++ scripts/download_wasms.sh
@@ -4,26 +4,8 @@ set -x
 
 WASM_FOLDER=".wasm"
 
-VAULT_VERSION="0.2.1"
-VAULT_URL="https://github.com/smart-social-contracts/kybra-simple-vault/releases/download/v${VAULT_VERSION}/"
 LEDGER_SUITE_URL="https://github.com/dfinity/ic/releases/download/ledger-suite-icrc-2025-02-27"
 mkdir -p $WASM_FOLDER
-
-
-# Download the vault files if they don't exist
-if [ ! -f "$WASM_FOLDER/vault.wasm" ]; then
-    echo "Downloading vault wasm file..."
-    curl -L -o "$WASM_FOLDER/vault.wasm" "${VAULT_URL}vault_${VAULT_VERSION}.wasm"
-else
-    echo "Vault wasm file already downloaded"
-fi
-if [ ! -f "$WASM_FOLDER/vault.did" ]; then
-    echo "Downloading vault did file..."
-    curl -L -o "$WASM_FOLDER/vault.did" "${VAULT_URL}vault_${VAULT_VERSION}.did"
-else
-    echo "Vault did file already downloaded"
-fi
-
 
 # Download the ledger files if they don't exist
 if [ ! -f "$WASM_FOLDER/ledger.wasm" ]; then
