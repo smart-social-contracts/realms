b01d69c78a984ee8bdf9784e8b0f150d0e6a966e
deucalioncodes <jmpmonteagudo@gmail.com>
2025-11-22 21:19:58 +0100

    fix: update DEFAULT_DOCKER_IMAGE to use latest tag with TODO note

--- cli/realms_cli/constants.py
+++ cli/realms_cli/constants.py
@@ -6,7 +6,7 @@ MAX_BATCH_SIZE = 100
 
 # Docker image configuration
 # Can be overridden with REALMS_DOCKER_IMAGE environment variable
-DEFAULT_DOCKER_IMAGE = "ghcr.io/smart-social-contracts/realms:3320d72c4e9a5ca2436b867c33a9337db74eaa37"
+DEFAULT_DOCKER_IMAGE = "ghcr.io/smart-social-contracts/realms:latest"  # TODO: this should be the image built in the latest release commit
 DOCKER_IMAGE = os.environ.get("REALMS_DOCKER_IMAGE", DEFAULT_DOCKER_IMAGE)
 
 # Default realm folder
