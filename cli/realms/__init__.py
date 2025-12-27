4fef6f083413278c147af9a72d1297c3b072877b
GitHub Action <action@github.com>
2025-12-27 16:30:11 +0000

    Bump version to 0.2.2

--- cli/realms/__init__.py
+++ cli/realms/__init__.py
@@ -18,5 +18,5 @@ from . import realm as realm
 from . import mundus as mundus
 from . import registry as registry
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __all__ = ["realm", "mundus", "registry", "__version__"]
