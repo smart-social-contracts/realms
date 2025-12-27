0888da812c84eb90077b8b2fb805e4ac0ab3b871
GitHub Action <action@github.com>
2025-12-27 15:48:15 +0000

    Bump version to 0.2.1

--- cli/realms/cli/constants.py
+++ cli/realms/cli/constants.py
@@ -7,7 +7,7 @@ MAX_BATCH_SIZE = 25  # Reduced from 100 to avoid ICP instruction limit (40B) on 
 # Docker image configuration
 # Updated automatically during release to match the current version
 # Can be overridden with REALMS_DOCKER_IMAGE environment variable
-DEFAULT_DOCKER_IMAGE = "ghcr.io/smart-social-contracts/realms:v0.1.5"
+DEFAULT_DOCKER_IMAGE = "ghcr.io/smart-social-contracts/realms:v0.2.1"
 DOCKER_IMAGE = os.environ.get("REALMS_DOCKER_IMAGE", DEFAULT_DOCKER_IMAGE)
 
 # Default realm folder
