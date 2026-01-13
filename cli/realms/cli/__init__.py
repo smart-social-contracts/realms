ffd310c853d3783aeb30d52bceecfb245917558a
deucalioncodes <jmpmonteagudo@gmail.com>
2026-01-13 18:23:28 +0100

    chore: bump version to 0.2.6

--- cli/realms/cli/__init__.py
+++ cli/realms/cli/__init__.py
@@ -1,5 +1,5 @@
 """Realms CLI - A tool for managing Realms project lifecycle."""
 
-__version__ = "0.2.5"
-__commit__ = "f5b6b99"
-__commit_date__ = "2026-01-13T12:47:31+01:00"
+__version__ = "0.2.6"
+__commit__ = "863218c"
+__commit_date__ = "2026-01-13T18:22:53+01:00"
