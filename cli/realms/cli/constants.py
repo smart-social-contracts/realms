534af6a642a1a9be38725091b04dfed8bc99dfd3
GitHub Action <action@github.com>
2026-02-02 05:47:07 +0000

    Bump version to 0.2.8

--- cli/realms/cli/constants.py
+++ cli/realms/cli/constants.py
@@ -7,7 +7,7 @@ MAX_BATCH_SIZE = 10  # Reduced from 25 to handle complex records with relationsh
 # Docker image configuration
 # Updated automatically during release to match the current version
 # Can be overridden with REALMS_DOCKER_IMAGE environment variable
-DEFAULT_DOCKER_IMAGE = "ghcr.io/smart-social-contracts/realms:v0.2.7"
+DEFAULT_DOCKER_IMAGE = "ghcr.io/smart-social-contracts/realms:v0.2.8"
 DOCKER_IMAGE = os.environ.get("REALMS_DOCKER_IMAGE", DEFAULT_DOCKER_IMAGE)
 
 # Default realm folder
